2eb983d0649e3780410fc646632168ad3bacac07
Felipe <fm043445@gmail.com>
2024-10-07 09:16:34 -0300

    Ajustes em Home e Produto

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Nav from '../src/components/Nav/Nav'
 import Footer from '../src/components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
       <Nav/>
+      <Outlet/>
       <Footer/>
     </>
   )
